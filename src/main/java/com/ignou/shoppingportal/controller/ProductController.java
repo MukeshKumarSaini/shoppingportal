4352dbd51cd38d3d5731fd085334482da9a10e9d
MukeshKumarSaini <47445781+MukeshKumarSaini@users.noreply.github.com>
2019-02-08 16:49:45 +0530

    Add files via upload

---
+++ src/main/java/com/ignou/shoppingportal/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.ignou.shoppingportal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	
+	@RequestMapping(value = "/product",method = RequestMethod.GET)
+	@ResponseBody
+	public String getProduct(){
+		return "Spring mvc Apllication project";
+
+	
+		
+	}
+
+}
+
