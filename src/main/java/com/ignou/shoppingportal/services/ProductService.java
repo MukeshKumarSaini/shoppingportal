3738f1ea833b19695419a93da536acac4ac9947e
MukeshKumarSaini <mukeshsaini8793@gmail.com>
2019-02-26 16:38:29 +0530

    logger initialize

--- src/main/java/com/ignou/shoppingportal/services/ProductService.java
+++ src/main/java/com/ignou/shoppingportal/services/ProductService.java
@@ -5,15 +5,21 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ignou.shoppingportal.controller.ProductController;
 import com.ignou.shoppingportal.model.Product;
 
 @Service
 public class ProductService {
 	@Autowired
 	EntityManager entityManager;
+	
+	private static Logger logger = Logger.getLogger(ProductService.class);
+	
 
 	public boolean addProduct(Product product) {
 		entityManager.getTransaction().begin();
@@ -38,6 +44,7 @@ public class ProductService {
 		Product product = null;
 		entityManager.getTransaction().begin();
 		product = entityManager.find(Product.class, productId);
+		  entityManager.getTransaction().commit();
 		return product;
 	}
 
@@ -45,6 +52,7 @@ public class ProductService {
 		entityManager.getTransaction().begin();
 		
 		  List<Product> product=new ArrayList<Product>();
+		  logger.info("Return all product");
 		  product= entityManager.createQuery("select e from Product e").getResultList();
 		  entityManager.getTransaction().commit();
 		  return product;
